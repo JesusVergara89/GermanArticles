4d62698881e6531b38f234ac3a8656525196d5c3
Jesus Vergara <jesusvergara@Jesuss-MacBook-Air.local>
2026-08-01 20:09:56 -0600

    new file supabase file to deploy in Netlify 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { supabase } from "./lib/supabaseClient";
+import { supabase } from "./Lib/supabaseClient";
 import FlashcardArtikel from "./Components/FlashcardArtikel";
 
 function App() {
